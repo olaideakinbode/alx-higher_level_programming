54565b936839e55fc421f81d75079d7f7273d496
Olaide Akinbode <olaideakinbode@gmail.com>
2023-03-21 03:59:18 -0700

    more projects on data structures especially lists, sets, tuples and dictionaries

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1]
+
+    return (num / den)
